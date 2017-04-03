056d5848550fa39f86ce5b8ddd044006a730e23e
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2017-04-03 19:44:50 +0100

    Add simple class examples.

---
+++ 04classes/bank_account.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# bank_account.py
+#
+# Simple Bank Account class example.
+#
+# AMJ
+# 2017-04-01
+
+class BankAccount:
+
+    def __init__ (self, account_number, account_holder, has_overdraft):
+        self.account_number = account_number
+        self.account_holder = account_holder
+        self.has_overdraft = has_overdraft
+
+        self.__balance = 0.0
+
+    @property
+    def balance (self):
+        return self.__balance
+
+    def deposit (self, deposit_amount):
+        try:
+            if deposit_amount > 0:
+                self.__balance += deposit
+        except TypeError:
+            pass
+
+    def withdraw (self, withdraw_amount):
+        try:
+            if withdraw_amount >= self.__balance or has_overdraft:
+                self.__balance -= withdraw_amount
+        except TypeError:
+            pass
+
+    def __str__ (self):
+        return "Account: {:} Holder: {:} Balance: {:}".format (self.account_number, self.account_holder, self.balance)
---
+++ 04classes/league_team.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+# league_team.py
+#
+# League table class example.
+#
+# AMJ
+# 2017-04-01
+
+class LeagueTable:
+
+    def __init__ (self, name):
+
+        self.name = name
+
+        self.__games_won = 0
+        self.__games_drawn = 0
+        self.__games_lost = 0
+
+        self.__points_for = 0
+        self.__points_against = 0
+
+    @property
+    def games_played (self):
+        return self.__games_won + self.__games_drawn + self.__games_lost
+
+    @property
+    def games_won (self):
+        return self.__games_won
+
+    @property
+    def games_drawn (self):
+        return self.__games_drawn
+
+    @property
+    def games_lost (self):
+        return self.__games_lost
+
+    @property
+    def points_for (self):
+        return self.__points_for
+
+    @property
+    def points_against (self):
+        return self.__points_against
+
+    @property
+    def points_difference (self):
+        return self.points_for - self.points_against
+
+    @property
+    def league_points (self):
+        return 3 * self.__games_won + self.__games_drawn
+
+    def play_game (self, scored, conceded):
+        self.__points_for += scored
+        self.__points_against += conceded
+
+        if scored > conceded:
+            self.__games_won += 1
+        elif conceded > scored:
+            self.__games_lost += 1
+        else:
+            self.__games_drawn += 1
+
+    def __str__ (self):
+        return "{:20s} {:2d} {:2d} {:2d} {:2d} {:3d} {:3d} {:-3d} {:2d}" \
+            .format (self.name,
+                     self.games_played, self.games_won, self.games_drawn, self.games_lost,
+                     self.points_for, self.points_against, self.points_difference,
+                     self.league_points)
+
+if __name__ == '__main__':
+
+    team_one = LeagueTable ('Huddersfield')
+    team_two = LeagueTable ('Wigan')
+
+    team_one.play_game (12, 8)
+    team_two.play_game (10, 24)
+
+    print (team_one)
+    print (team_two)
